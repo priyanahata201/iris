f101f97e5cea79ad4763674fcb6f8ce9d79bcae7
priyanahata201 <priya.nahata@hoonartek.com>
2025-05-19 11:45:12 +0530

    Update iris_app.py

--- iris_app.py
+++ iris_app.py
@@ -19,8 +19,8 @@ if uploaded_file is not None:
     st.subheader("Dataset Preview")
     st.write(df.head())
 
-    X = df.drop('Species', axis=1)
-    y = df['Species']
+    X = df.drop('species', axis=1)
+    y = df['species']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
