78728636f6a25250830c956a6ae4dcb45b8a0e27
priyanahata201 <priya.nahata@hoonartek.com>
2025-05-19 12:13:34 +0530

    Update diabetes_dataset.py

--- diabetes_dataset.py
+++ diabetes_dataset.py
@@ -53,10 +53,10 @@ if uploaded_file is not None:
     Age = st.number_input("Age", value=51)
 
     if st.button("Predict Outcome"):
-    input_data = pd.DataFrame([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]], columns=X.columns)
-    prediction = model.predict(input_data)[0]
-    result = "Diabetic" if prediction == 1 else "Not Diabetic"
-    st.success(f"Predicted Outcome: **{result}**")
+        input_data = pd.DataFrame([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]], columns=X.columns)
+        prediction = model.predict(input_data)[0]
+        result = "Diabetic" if prediction == 1 else "Not Diabetic"
+        st.success(f"Predicted Outcome: **{result}**")
     
 else:
     st.info("Please upload a CSV file to proceed.")
